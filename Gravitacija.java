14e9c31d01d693fd27c9e82b6a9b8b7e18b1cac0
jure213 <jure.rus04@gmail.com>
2024-02-27 17:50:40 +0100

    dodan izračun pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,10 +3,19 @@
  */
 public class Gravitacija {
 
+    public static final double GRAV_KONST = 6.674e-11;
+    public static final double MASA_ZEMLJE = 5.972e24;
+    public static final double POLMER_ZEMLJE = 6.371e6;
+
     public static void main(String[] args) {
         
         System.out.println("Test!");
 
+    }
+
+    public static double gravPospesekZVisino(int visina) {
+        double razdaljaOdSredisca = visina + POLMER_ZEMLJE;
+        return (GRAV_KONST * MASA_ZEMLJE)/(razdaljaOdSredisca*razdaljaOdSredisca);
     }
 
 }
